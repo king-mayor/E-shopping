3a7b9607904e70fbd1200d9a70bd4ef5897f9c62
king-mayor <99553727+king-mayor@users.noreply.github.com>
2024-09-19 16:28:28 -0700

    updated cart design

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -13,22 +13,22 @@ const CartProduct = ({ prod }) => {
   return (
     <>
       <div className="">
-        <div className="flex md:flex-row flex-col justify-around  items-center my-6 md:gap-0 gap-4">
-          <div className="flex items-center gap-4">
+        <div className="flex flex-row justify-around  items-center my-6 md:gap-0 gap-4">
+          <div className="lg:flex lg:flex-row flex-col items-center gap-4">
             <div>
-              <img className="w-28 h-28" src={image} alt="cart-img" />
+              <img
+                className="lg:w-28 lg:h-28 w-10 h-10"
+                src={image}
+                alt="cart-img"
+              />
             </div>
             <div>
-              <h2 className="text-xl font-bold text-black">{name}</h2>
-              <button
-                className="text-gray-100 bg-red-600 p-2 cursor-pointer border-none outline-none my-2"
-                onClick={() => removeFromCart(id)}
-              >
-                Delete
-              </button>
+              <h2 className="lg:text-xl text-md font-bold text-black w-10">
+                {name}
+              </h2>
             </div>
           </div>
-          <div className="flex gap-4">
+          <div className="flex">
             <div>
               <button
                 className="text-gray-100 bg-black font-bold cursor-pointer outline-none border-none p-2"
@@ -50,22 +50,32 @@ const CartProduct = ({ prod }) => {
             </div>
           </div>
           <div>
-            <p className="text-xl text-gray-600">
-              <span className="text-xl font-bold text-black">Price:</span> $
-              {price}
+            <p className="lg:text-xl text-md text-gray-600">
+              <span className="lg:text-xl text-md font-bold text-black pr-1">
+                Price:
+              </span>
+              ${price}
             </p>
           </div>
-          <div>
+          {/* <div>
             <p className="text-xl text-gray-600">
               <span className="text-xl font-bold text-black">Total:</span> $150
             </p>
-          </div>
-          <div>
+          </div> */}
+          {/* <div>
             <button
               className="text-gray-100 bg-red-600 p-2 cursor-pointer border-none outline-none my-2"
               onClick={clearCart}
             >
               clear cart
+            </button>
+          </div> */}
+          <div>
+            <button
+              className="text-gray-100 bg-red-600 p-2 cursor-pointer border-none outline-none my-2"
+              onClick={() => removeFromCart(id)}
+            >
+              Delete
             </button>
           </div>
         </div>
